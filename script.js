5106043cb430fa2f9217baa64ccc12cce74af5fc
vemavarapu-daniel <71017366+vemavarapu-daniel@users.noreply.github.com>
2024-06-08 21:28:16 +0530

    added additional functionalities
    !. reset score
    2. Current score and total score

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ const player1Name = document.querySelector("name--1");
 const player1Score = document.querySelector("score--1");
 const CurrentScore = document.querySelectorAll(".current-score");
 var currentScore0 = document.getElementById("current--0");
+var currentScore1=  document.getElementById("current--1");
+var Score0 = document.getElementById("score--0");
+var Score1 = document.getElementById("score--1");
 
 const player0 = document.querySelector(".player--0");
 const player1 = document.querySelector(".player--1");
 let vplayer0TScore = 0; //total score
-let vplayer1Score = 0;
-
+let vplayer1TScore = 0;
 let vplayer0CScore = 0; //current score
 let vplayer1CScore = 0;
 
@@ -24,15 +26,19 @@ const dice = document.querySelector(".dice");
 
 dice.classList.add("hidden");
 
-console.log(currentScore0.innerHTML);
+//console.log(currentScore0.innerHTML);
 
 //Who plays
 let activePlayer = 0;
 
 function switchPlayer() {
-  //  ------test to check if switching when starting is 1
-  //let active_player = 1;
+
   console.log("Hold Pressed");
+
+  Score0.textContent = vplayer0TScore //updating total score
+  Score1.textContent = vplayer1TScore //updating total score
+ 
+
   // Remove active class from current active player
   document
     .querySelector(`.player--${activePlayer}`)
@@ -43,35 +49,69 @@ function switchPlayer() {
   document
     .querySelector(`.player--${activePlayer}`)
     .classList.add("player--active");
+
 }
-
-//roll dice
-
-//current score function
 
 //On_one function
 
-function current_Score() {}
+function onHold() {
+
+ if (activePlayer === 0) {
+    vplayer0TScore += vplayer0CScore;
+    vplayer0CScore = 0; // Reset current score to 0 on hold
+  } else {
+    vplayer1TScore += vplayer1CScore;
+    vplayer1CScore = 0; // Reset current score to 0 on hold
+  }
+
+  console.log(vplayer0CScore);
+  console.log(vplayer0TScore);
+
+  currentScore0.textContent = vplayer0CScore; // Update displayed current score to 0 
+  currentScore1.textContent = vplayer1CScore; // Update displayed current score to 0
+
+  console.log(
+    `total score 1- ${vplayer0TScore} ,, tota score 2 - ${vplayer1TScore}`
+  );
+
+  switchPlayer();
+
+ 
+}
 
 function resetScore() {
-  document.querySelector(".dice").src = `/dice-5.png`;
+  document.querySelector(".dice").src = `dice-5.png`;
   activePlayer = 1;
-  let vplayer0CScore = 0;
-  let vplayer1CScore = 0;
+
+
+    vplayer0CScore = 0;
+    vplayer1CScore = 0;
+    vplayer1TScore = 0;
+    vplayer0TScore = 0;
+
   for (let i = 0; i < 2; i++) {
-    document.getElementById(`current--${i}`).textContent = vplayer0CScore;
-    document.getElementById(`score--${i}`).textContent = vplayer0CScore;
+    document.getElementById(`current--${i}`).textContent = 0;
+    document.getElementById(`score--${i}`).textContent = 0;
+
   }
+  console.log (`total score 1- ${vplayer0TScore} ,, tota score 2 - ${vplayer1TScore}`)
+  console.log("entering into switch function")
+  //document.getElementsByClassName("score").querySelectorAll.textContent=0;
+ 
 
   switchPlayer();
 }
+
 
 //roll dice on click.
 
 btnRollDice.addEventListener("click", function () {
   let randomNum = Math.trunc(Math.random() * 6 + 1);
-  document.querySelector(".dice").src = `/dice-${randomNum}.png`;
+
+  document.querySelector(".dice").src = `dice-${randomNum}.png`; 
   console.log(randomNum);
+
+
   if (activePlayer === 0) {
     console.log("player 1 ");
     vplayer0CScore += randomNum;
@@ -80,10 +120,12 @@ btnRollDice.addEventListener("click", function () {
     vplayer1CScore += randomNum;
   }
 
+
   document.getElementById("current--0").textContent = vplayer0CScore;
   document.getElementById("current--1").textContent = vplayer1CScore;
+  document.getElementById("current--1").textContent = vplayer1CScore;
 
-  console.log(`player1 current`);
+
 });
 
 //test
@@ -91,4 +133,4 @@ btnRollDice.addEventListener("click", function () {
 //test
 
 btnNewGame.addEventListener("click", resetScore);
-BtnHoldScore.addEventListener("click", switchPlayer);
+BtnHoldScore.addEventListener("click", onHold);
